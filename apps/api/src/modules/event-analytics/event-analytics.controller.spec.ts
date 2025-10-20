01d365d6367760cb05a87e948190b871299e8fb2
joseAcevesG <jose.aceves@iteso.mx>
2025-10-20 17:46:25 -0600

    test[SCRUM-69]: add mock services and database connection to event analytics tests

--- apps/api/src/modules/event-analytics/event-analytics.controller.spec.ts
+++ apps/api/src/modules/event-analytics/event-analytics.controller.spec.ts
@@ -1,15 +1,23 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { EventAnalyticsController } from "./event-analytics.controller";
+import { EventAnalyticsService } from "./event-analytics.service";
 
 describe("EventAnalyticsController", () => {
 	let controller: EventAnalyticsController;
 
+	const mockEventAnalyticsService = {
+		getParticipants: jest.fn(),
+		getChart: jest.fn(),
+	};
+
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			controllers: [EventAnalyticsController],
+			providers: [{ provide: EventAnalyticsService, useValue: mockEventAnalyticsService }],
 		}).compile();
 
 		controller = module.get<EventAnalyticsController>(EventAnalyticsController);
+		jest.clearAllMocks();
 	});
 
 	it("should be defined", () => {
